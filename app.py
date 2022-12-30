557ef01eeeddee29ffd799cfea38eca83958a658
serpenta <ignisserpenta@gmail.com>
2022-12-30 21:06:09 +0100

    added first iteration of flow design

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-print('Hello World!')
+"""
+Menu:
+- user management
+    - select user
+    - create user
+    - delete user
+- account management
+    - create account
+    - rename account
+    - remove account
+    - print accounts
+    - finances overview
+- category management
+    - create category
+    - rename category
+    - remove category
+    - print categories
+- select account
+    - print balance
+    - print operation history
+        - time constrained
+            - month
+            - year
+        - category constrained
+    - add transaction
+    - modify transaction
+    - remove transaction
+- add transaction
+"""
